f62a86379e31ff721efd7e773c346400e63a8875
thwang <htw1203@gmail.com>
2023-03-15 18:07:06 +0900

    2023-03-15 스프링

---
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        //enum type은 == 을 사용한다.
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else return 0;
+    }
+}
