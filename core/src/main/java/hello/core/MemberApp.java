795ea51b98783bf698b7ce0132f4fd2ac4cef8a0
thwang <htw1203@gmail.com>
2023-04-05 23:20:12 +0900

    2023-04-05

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -20,7 +20,7 @@ public class MemberApp {
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
-        System.out.println("new  member = " + member.getName());
-        System.out.println("findMember = " + findMember.getName());
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
     }
 }
